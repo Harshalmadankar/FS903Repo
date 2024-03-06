2217995cdca8fc80567ab1ef4622e327609cb07b
Admin <Admin@LAPTOP-IL4M4OED>
2024-03-06 19:09:32 +0530

    task1---dev11

--- GitProj01/src/main/java/com/nt/main/Apptest.java
+++ GitProj01/src/main/java/com/nt/main/Apptest.java
@@ -10,6 +10,7 @@ public class Apptest
 		System.out.println("sum:"+ar.sum(100,200));
 		System.out.println("sub:"+ar.sub(100,200));
 		System.out.println("=============");
+		System.out.println("-------------");
 		
  		
 	}
