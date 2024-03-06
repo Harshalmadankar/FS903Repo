a96437f784fb84e6e5ddde2de0aad69c9c702d92
Admin <Admin@LAPTOP-IL4M4OED>
2024-03-06 19:02:19 +0530

    task1 from dev1

--- GitProj01/src/main/java/com/nt/main/Apptest.java
+++ GitProj01/src/main/java/com/nt/main/Apptest.java
@@ -1,9 +1,17 @@
 package com.nt.main;
 
+import com.nt.service.ArithmeticOperations;
+
 public class Apptest
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		ArithmeticOperations ar=new ArithmeticOperations();
+		System.out.println("sum:"+ar.sum(100,200));
+		System.out.println("sub:"+ar.sub(100,200));
 		
+		
+ 		
 	}
 
 }
